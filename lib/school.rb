dc6f71a5d3839f5c7cc0817f55405e6cff81142a
DAZUN XING <dazun.xing@gmail.com>
2020-05-15 08:29:57 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+
+class School
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name =name
+    @roster ={}
+  end
+  
+  def add_student(name, grade)
+   if @roster.has_key?(grade)
+     @roster[grade] << name
+   else
+     @roster[grade] =[name]
+   end
+  end
+  
+  def grade(grade)
+    if @roster.has_key?(grade)
+      @roster[grade]
+    end
+  end
+      
+  def sort
+    @roster.each do |x, y|
+      y.sort!
+    end
+  end
+      
+
+  
+end

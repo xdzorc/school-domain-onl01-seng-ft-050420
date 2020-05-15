dcbb48654275e6e9661575517ea0b896c2a7dd80
DAZUN XING <dazun.xing@gmail.com>
2020-05-15 06:36:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
 # code here!
+
+class School
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name =name
+    @roster ={}
+  end
+  
+  def add_student(name, grade)
+    @roster[name] =[]
+    @roster[name] << grade
+
+  
+end
